3fd0f211502580192774e4b1348cf87d5b8e8c9e
nikkou <nikkou@writeme.com>
2014-01-18 15:36:30 +0400

    JavaFX transition

--- src/Tones.java
+++ src/Tones.java
@@ -1,56 +1,55 @@
-import org.gnome.gtk.*;
-import org.gnome.gdk.Event;
 import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.event.*;
 
-public class Tones extends Window {
+public class Tones extends Application {
 	public static void main(String args[]) {
-		Gtk.init(args);
-		new Tones(args);
+		launch(args);
 	}
 
-	private Entry frequencyEntry;
-	private Button playButton, stopButton;
 	private Player player;
 	private boolean playing = false;
 
-	public Tones(String[] args) {
-		hide();
-		setTitle("Tones");
-		destroyOnDelete();
-		setTonesIcon();
-
+	public void start(Stage primaryStage) {
 		player.init();
+		primaryStage.setTitle("Tones");
+		VBox vbox = new VBox();
 
-		VBox vbox = new VBox(false, 0);
-		vbox.add(frequencyEntry = new Entry());
-		HBox buttons = new HBox(false, 0);
-		buttons.add(playButton = new Button("Play"));
-		playButton.connect(new Button.Clicked() {
-			public void onClicked(Button button) {
-				if(!playing) {
-					float[] freqs = parseFreqs(frequencyEntry.getText());
+		final TextField frequencyField = new TextField();
+		vbox.getChildren().add(frequencyField);
+
+        HBox hbox = new HBox();
+		Button playButton = new Button("Play");
+		playButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                if(!playing) {
+					float[] freqs = parseFreqs(frequencyField.getText());
 					if(freqs.length > 0) {
 						player = new Player(freqs);
 						playing = true;
 					}
 				}
-			}
-		});
-		buttons.add(stopButton = new Button("Stop"));
-		stopButton.connect(new Button.Clicked() {
-			public void onClicked(Button button) {
-				if(player != null) {
+            }
+        });
+        hbox.getChildren().add(playButton);
+		Button stopButton = new Button("Stop");
+		stopButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                if(player != null) {
 					player.stopSound();
 					playing = false;
 				}
-			}
-		});
-		vbox.add(buttons);
-
-		add(vbox);
-		showAll();
-		setCenterLocation();
-		Gtk.main(); //include windows and mac gtk native libraries
+            }
+        });
+        hbox.getChildren().add(stopButton);
+        vbox.getChildren().add(hbox);
+        
+        primaryStage.setScene(new Scene(vbox));
+        primaryStage.show();
 	}
 
 	private float[] parseFreqs(String text) {
@@ -67,30 +66,5 @@ public class Tones extends Window {
 			freqsArray[i] = freqsList.get(i);
 		}
 		return freqsArray;
-	}
-
-	public void destroyOnDelete() {
-		connect(new Window.DeleteEvent() {
-			public boolean onDeleteEvent(Widget source, Event event) {
-				source.destroy();
-				return false;
-			}
-		});
-	}
-
-	private void setTonesIcon() {
-		//find icon, put it in ./ico and set it here
-		// try {
-		// 	Pixbuf icon = new Pixbuf("ico/" + ico);
-		// 	w.setIcon(icon);
-		// } catch(Exception ex) {ex.printStackTrace();}
-	}
-
-	private void setCenterLocation() {
-		int sw = getScreen().getWidth();
-		int sh = getScreen().getHeight();
-		int x = (sw - getWidth()) / 2;
-		int y = (sh - getHeight()) / 2;
-		move(x, y);
 	}
 }
